b1e9a49c1574c24343788b5f3ec60944925cda75
piusprince <45078705+piusprince@users.noreply.github.com>
2021-11-26 14:25:34 +0000

    Update auth.ts
    add express validator for validating email and password

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,42 @@
-import express from 'express'
-import { body, validationResult } from 'express-validator'
+import express from "express";
+import { body, validationResult } from "express-validator";
+import User from "../models/user";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup', body("email").isEmail(),body("password").isLength({min: 5}), async (req, res ) => {
-    const validationErrors = validationResult(req)
-    const {email, password} = req.body;
-    res.json({email, password})
-})
+router.post(
+  "/signup",
+  body("email").isEmail().withMessage("The email is invalid"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("The password is too short"),
+  async (req, res) => {
+    const validationErrors = validationResult(req);
 
-export default router
+    if (!validationErrors.isEmpty()) {
+      const errors = validationErrors.array().map((error) => error.msg);
+      return res.json({ errors, data: null });
+    }
+
+    const { email, password } = req.body;
+
+    await User.create({ email, password });
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.json({
+        errors: [
+          {
+            msg: "Email already exists",
+          },
+        ],
+        data: null,
+      });
+    }
+
+    res.json(user);
+  }
+);
+
+export default router;
