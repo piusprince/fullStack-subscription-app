19ae99fc65bae3d5595fe46b0412be238967e457
piusprince <45078705+piusprince@users.noreply.github.com>
2021-11-26 14:27:20 +0000

    Update index.ts
    added mongose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,23 @@
-import express from 'express'
-import authRoutes from './routes/auth'
+import express from "express";
+import authRoutes from "./routes/auth";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.use(express.json())
-app.use("/auth", authRoutes)
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080')
-})
+    const app = express();
+
+    app.use(express.json());
+    app.use("/auth", authRoutes);
+    app.listen(8080, () => {
+      console.log("Listening on port 8080");
+    });
+  })
+  .catch((error) => {
+    throw new Error(error);
+  });
